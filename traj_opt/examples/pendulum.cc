10ee861fa39807465045ee14b55f4c668d1a575f
Vince Kurtz <vjkurtz@gmail.com>
2022-07-21 16:58:47 -0400

    add problem data structs

--- traj_opt/examples/pendulum.cc
+++ traj_opt/examples/pendulum.cc
@@ -2,6 +2,7 @@
 
 #include "drake/traj_opt/solution_data.h"
 #include "drake/traj_opt/problem_definition.h"
+#include "drake/traj_opt/problem_data.h"
 
 namespace drake {
 namespace traj_opt {
@@ -12,8 +13,10 @@ int do_main() {
   std::cout << "hello world" << std::endl;
 
   ProblemDefinition optimization_problem;
-
   (void) optimization_problem;
+  
+  ProblemData prob_data;
+  (void) prob_data;
 
   return 0;
 }
