582d2d83e2cd9a2d3bbfde9e57ca680e70f6dbe4
Vince Kurtz <vjkurtz@gmail.com>
2022-07-23 13:59:57 -0400

    refactor as unit test

--- traj_opt/examples/pendulum.cc
+++ traj_opt/examples/pendulum.cc
@@ -66,76 +66,9 @@ void run_passive_simulation(double time_step, double sim_time) {
   simulator.AdvanceTo(sim_time);
 }
 
-/**
- * Test our computation of generalized velocities
- *
- *   v_t = (q_t - q_{t-1})/dt
- *
- */
-void test_v_from_q() {
-  // Set up the system model
-  // TODO(vincekurtz): set SAP as discrete contact solver
-  auto plant = std::make_unique<MultibodyPlant<double>>(1e-2);
-  const std::string urdf_file =
-      FindResourceOrThrow("drake/traj_opt/examples/pendulum.urdf");
-  Parser(plant.get()).AddAllModelsFromFile(urdf_file);
-  plant->Finalize();
-  auto plant_context = plant->CreateDefaultContext();
-
-  // Simulate forward, recording the state
-  int num_steps = 5;
-  MatrixXd x(plant->num_multibody_states(), num_steps + 1);
-  auto q = x.topRows<1>();
-  auto v = x.bottomRows<1>();
-
-  auto x0 = x.leftCols<1>();  // set initial state
-  x0 << 1.3, 0.4;
-  plant->SetPositionsAndVelocities(plant_context.get(), x.leftCols<1>());
-
-  auto state = plant->AllocateDiscreteVariables();
-  for (int i = 0; i < num_steps; ++i) {
-    // Use some non-zero actuations to make things more interesting
-    plant->get_actuation_input_port().FixValue(plant_context.get(),
-                                               sin(plant->time_step() * i));
-
-    plant->CalcDiscreteVariableUpdates(*plant_context, state.get());
-    plant_context->SetDiscreteState(state->get_value());
-    x.col(i + 1) = state->get_value();
-  }
-
-  // Create a trajectory optimizer
-  ProblemDefinition opt_prob;
-  opt_prob.q0 = q.col(0);
-  opt_prob.v0 = v.col(0);
-  opt_prob.T = num_steps;
-  TrajectoryOptimizer optimizer(std::move(plant), opt_prob);
-
-  // Trajectory optimizer computes v from q
-  std::vector<VectorXd> q_vector;
-  for (int i = 0; i <= num_steps; ++i) {
-    q_vector.emplace_back(q.col(i));
-  }
-
-  std::vector<VectorXd> v_vector(num_steps + 1);
-  optimizer.CalcV(q_vector, &v_vector);
-
-  // Check that our computed v matches the recorded v
-  MatrixXd v_computed(v.rows(), v.cols());
-  for (int i = 0; i <= num_steps; ++i) {
-    v_computed.col(i) = v_vector[i];
-  }
-
-  std::cout << v << std::endl;
-  std::cout << v_computed << std::endl;
-  std::cout << v - v_computed << std::endl;
-}
-
 int do_main() {
   // For now we'll just run a simple passive simulation of the pendulum
-  // run_passive_simulation(1e-2, 2.0);
-
-  // Test computation of v from q
-  test_v_from_q();
+  run_passive_simulation(1e-2, 2.0);
 
   return 0;
 }
