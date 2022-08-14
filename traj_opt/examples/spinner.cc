442067e247af3ec2e8e4105e2deb218c984684c3
Vince Kurtz <vjkurtz@gmail.com>
2022-08-14 11:25:45 -0400

    add gflags option for spinner geometry

--- traj_opt/examples/spinner.cc
+++ traj_opt/examples/spinner.cc
@@ -1,3 +1,5 @@
+#include <gflags/gflags.h>
+
 #include "drake/common/find_resource.h"
 #include "drake/multibody/parsing/parser.h"
 #include "drake/multibody/plant/multibody_plant.h"
@@ -8,13 +10,35 @@ namespace traj_opt {
 namespace examples {
 namespace spinner {
 
+DEFINE_string(geometry, "sphere",
+              "Geometry of the spinner. Options are 'rectangle', 'capsule', "
+              "'square', or 'sphere'.");
+
 using multibody::MultibodyPlant;
 using multibody::Parser;
 
 class SpinnerExample : public TrajOptExample {
   void CreatePlantModel(MultibodyPlant<double>* plant) const {
-    const std::string urdf_file =
-        FindResourceOrThrow("drake/traj_opt/examples/spinner.urdf");
+    // N.B. geometry of the spinner is chosen via gflags rather than yaml so
+    // that we can use the same yaml format for all of the examples, without
+    // cluttering it with spinner-specific options.
+    std::string urdf_file;
+    if (FLAGS_geometry == "rectangle") {
+      urdf_file =
+          FindResourceOrThrow("drake/traj_opt/examples/spinner_rectangle.urdf");
+    } else if (FLAGS_geometry == "capsule") {
+      urdf_file =
+          FindResourceOrThrow("drake/traj_opt/examples/spinner_capsule.urdf");
+    } else if (FLAGS_geometry == "square") {
+      urdf_file =
+          FindResourceOrThrow("drake/traj_opt/examples/spinner_square.urdf");
+    } else if (FLAGS_geometry == "sphere") {
+      urdf_file =
+          FindResourceOrThrow("drake/traj_opt/examples/spinner_sphere.urdf");
+    } else {
+      throw std::runtime_error(
+          fmt::format("Unknown spinner geometry '{}'.", FLAGS_geometry));
+    }
     Parser(plant).AddAllModelsFromFile(urdf_file);
   }
 };
@@ -24,7 +48,9 @@ class SpinnerExample : public TrajOptExample {
 }  // namespace traj_opt
 }  // namespace drake
 
-int main() {
+int main(int argc, char* argv[]) {
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+
   drake::traj_opt::examples::spinner::SpinnerExample spinner_example;
   spinner_example.SolveTrajectoryOptimization(
       "drake/traj_opt/examples/spinner.yaml");
