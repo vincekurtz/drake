b6aff9bdf1f5a4981d0c67b2288373a9863b7eee
Vince Kurtz <vjkurtz@gmail.com>
2022-09-08 15:57:40 -0400

    simple 90 degree rotation example

--- traj_opt/examples/allegro_hand.cc
+++ traj_opt/examples/allegro_hand.cc
@@ -28,42 +28,42 @@ class AllegroHandExample : public TrajOptExample {
     const Vector4<double> black(0.0, 0.0, 0.0, 0.5);
 
     // Add a model of the hand
-    std::string sdf_file = FindResourceOrThrow(
-        "drake/traj_opt/examples/allegro_hand.sdf");
+    std::string sdf_file =
+        FindResourceOrThrow("drake/traj_opt/examples/allegro_hand.sdf");
     Parser(plant).AddAllModelsFromFile(sdf_file);
-    RigidTransformd X_hand(RollPitchYawd(0,-M_PI_2, 0), Vector3d(0, 0, 0));
-    plant->WeldFrames(plant->world_frame(), plant->GetFrameByName("hand_root"), X_hand);
-
+    RigidTransformd X_hand(RollPitchYawd(0, -M_PI_2, 0), Vector3d(0, 0, 0));
+    plant->WeldFrames(plant->world_frame(), plant->GetFrameByName("hand_root"),
+                      X_hand);
 
     // Add a free-floating ball
-     ModelInstanceIndex ball_idx = plant->AddModelInstance("ball");
+    ModelInstanceIndex ball_idx = plant->AddModelInstance("ball");
 
-     const double mass = 0.05;
-     const double radius = 0.06;
+    const double mass = 0.05;
+    const double radius = 0.06;
 
-     const SpatialInertia<double> I(mass, Vector3d::Zero(),
-                                    UnitInertia<double>::SolidSphere(radius));
-     const RigidBody<double>& ball = plant->AddRigidBody("ball", ball_idx, I);
+    const SpatialInertia<double> I(mass, Vector3d::Zero(),
+                                   UnitInertia<double>::SolidSphere(radius));
+    const RigidBody<double>& ball = plant->AddRigidBody("ball", ball_idx, I);
 
-     plant->RegisterVisualGeometry(ball, RigidTransformd::Identity(),
-                                   Sphere(radius), "ball_visual", blue);
-     plant->RegisterCollisionGeometry(ball, RigidTransformd::Identity(),
-                                      Sphere(radius), "ball_collision",
-                                      CoulombFriction<double>());
+    plant->RegisterVisualGeometry(ball, RigidTransformd::Identity(),
+                                  Sphere(radius), "ball_visual", blue);
+    plant->RegisterCollisionGeometry(ball, RigidTransformd::Identity(),
+                                     Sphere(radius), "ball_collision",
+                                     CoulombFriction<double>());
 
     // Add some markers to the ball so we can see its rotation
-     RigidTransformd X_m1(RollPitchYawd(0, 0, 0), Vector3d(0, 0, 0));
-     RigidTransformd X_m2(RollPitchYawd(M_PI_2, 0, 0), Vector3d(0, 0, 0));
-     RigidTransformd X_m3(RollPitchYawd(0, M_PI_2, 0), Vector3d(0, 0, 0));
-     plant->RegisterVisualGeometry(ball, X_m1,
-                                   Cylinder(0.1 * radius, 2 * radius),
-                                   "ball_marker_one", black);
-     plant->RegisterVisualGeometry(ball, X_m2,
-                                   Cylinder(0.1 * radius, 2 * radius),
-                                   "ball_marker_two", black);
-     plant->RegisterVisualGeometry(ball, X_m3,
-                                   Cylinder(0.1 * radius, 2 * radius),
-                                   "ball_marker_three", black);
+    RigidTransformd X_m1(RollPitchYawd(0, 0, 0), Vector3d(0, 0, 0));
+    RigidTransformd X_m2(RollPitchYawd(M_PI_2, 0, 0), Vector3d(0, 0, 0));
+    RigidTransformd X_m3(RollPitchYawd(0, M_PI_2, 0), Vector3d(0, 0, 0));
+    plant->RegisterVisualGeometry(ball, X_m1,
+                                  Cylinder(0.1 * radius, 2 * radius),
+                                  "ball_marker_one", black);
+    plant->RegisterVisualGeometry(ball, X_m2,
+                                  Cylinder(0.1 * radius, 2 * radius),
+                                  "ball_marker_two", black);
+    plant->RegisterVisualGeometry(ball, X_m3,
+                                  Cylinder(0.1 * radius, 2 * radius),
+                                  "ball_marker_three", black);
   }
 };
 
