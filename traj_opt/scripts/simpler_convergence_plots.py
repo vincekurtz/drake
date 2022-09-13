72210a796c80bd46c5dfffb90e378af843800da4
Vince Kurtz <vjkurtz@gmail.com>
2022-09-13 12:02:59 -0400

    add contact parameters label to convergence comparison plot

--- traj_opt/scripts/simpler_convergence_plots.py
+++ traj_opt/scripts/simpler_convergence_plots.py
@@ -20,7 +20,7 @@ import sys
 drake_root = os.getcwd()
 
 # Bazel stores files in strange places
-data_file1 = drake_root + f"/bazel-out/k8-opt/bin/traj_opt/examples/spinner.runfiles/drake/solver_stats.csv"
+data_file1 = drake_root + f"/bazel-out/k8-opt/bin/traj_opt/examples/spinner.runfiles/drake/solver_stats_1.csv"
 data_file2 = drake_root + f"/bazel-out/k8-opt/bin/traj_opt/examples/spinner.runfiles/drake/solver_stats_2.csv"
 data_file3 = drake_root + f"/bazel-out/k8-opt/bin/traj_opt/examples/spinner.runfiles/drake/solver_stats_3.csv"
 
@@ -30,30 +30,30 @@ data2 = np.genfromtxt(data_file2, delimiter=',', names=True)
 data3 = np.genfromtxt(data_file3, delimiter=',', names=True)
 
 # Make plots
-fig, ax = plt.subplots(1,1,sharex=True,figsize=(6,6))
+fig, ax = plt.subplots(2,1,sharex=True,figsize=(6,6))
 plt.subplots_adjust(left=0.14,
                     bottom=0.08,
                     right=0.98,
-                    top=0.94,
+                    top=0.92,
                     wspace=0.1,
                     hspace=0.1)
 
-fig.suptitle(f"Spinner Convergence with $F=1 N$, $\delta=1 mm$, $v_s = 0.01 m/s$")
+fig.suptitle(f"Spinner Convergence from Different Initial Conditions\n$F=1N$, $\delta=1cm$, $v_s = 0.05m/s$")
 
-#ax[0].plot(data1["iter"], data1["cost"])
-#ax[0].plot(data2["iter"], data2["cost"])
-#ax[0].plot(data3["iter"], data3["cost"])
-#ax[0].set_ylabel("Cost")
-#ax[0].set_yscale("log")
+ax[0].plot(data1["iter"], data1["cost"])
+ax[0].plot(data2["iter"], data2["cost"])
+ax[0].plot(data3["iter"], data3["cost"])
+ax[0].set_ylabel("Cost")
+ax[0].set_yscale("log")
 
-ax.plot(data1["iter"], -data1["dL_dq"])
-#ax[1].plot(data2["iter"], -data2["dL_dq"])
-#ax[1].plot(data3["iter"], -data3["dL_dq"])
-ax.set_ylabel(r"Gradient ($\frac{|g' \Delta q|}{cost}$)")
-ax.set_yscale("log")
-ax.set_ylim(1e-16,1e1)
+ax[1].plot(data1["iter"], -data1["dL_dq"])
+ax[1].plot(data2["iter"], -data2["dL_dq"])
+ax[1].plot(data3["iter"], -data3["dL_dq"])
+ax[1].set_ylabel(r"Gradient ($\frac{|g' \Delta q|}{cost}$)")
+ax[1].set_yscale("log")
+ax[1].set_ylim(1e-16,1e1)
 
-ax.set_xlabel("Iteration")
+ax[1].set_xlabel("Iteration")
 
 
 plt.show()
