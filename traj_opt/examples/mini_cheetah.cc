5f2040e3163e2c29afcdd0fe37a8ef06869dffad
Vince Kurtz <vjkurtz@gmail.com>
2023-01-12 11:09:33 -0500

    add basic mini cheetah example

---
+++ traj_opt/examples/mini_cheetah.cc
@@ -0,0 +1,46 @@
+#include <drake/multibody/tree/prismatic_joint.h>
+
+#include "drake/common/find_resource.h"
+#include "drake/multibody/plant/multibody_plant.h"
+#include "drake/traj_opt/examples/example_base.h"
+
+namespace drake {
+namespace traj_opt {
+namespace examples {
+namespace mini_cheetah {
+
+using Eigen::Vector3d;
+using geometry::Box;
+using math::RigidTransformd;
+using multibody::CoulombFriction;
+using multibody::MultibodyPlant;
+using multibody::Parser;
+
+/**
+ * A model of the MIT mini cheetah quadruped.
+ */
+class MiniCheetahExample : public TrajOptExample {
+  void CreatePlantModel(MultibodyPlant<double>* plant) const {
+    // Add the robot
+    std::string urdf_file = FindResourceOrThrow(
+        "drake/traj_opt/examples/models/mini_cheetah_mesh.urdf");
+    Parser(plant).AddAllModelsFromFile(urdf_file);
+
+    // Add collision with the ground
+    RigidTransformd X_ground(Vector3d(0.0, 0.0, -5.0));
+    plant->RegisterCollisionGeometry(plant->world_body(), X_ground,
+                                     Box(25, 25, 10), "ground",
+                                     CoulombFriction<double>(0.5, 0.5));
+  }
+};
+
+}  // namespace mini_cheetah
+}  // namespace examples
+}  // namespace traj_opt
+}  // namespace drake
+
+int main() {
+  drake::traj_opt::examples::mini_cheetah::MiniCheetahExample example;
+  example.RunExample("drake/traj_opt/examples/mini_cheetah.yaml");
+  return 0;
+}
