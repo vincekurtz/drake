8b563ab93a4836cb0e922430e0d9d2ccb07d1a8c
Vince Kurtz <vjkurtz@gmail.com>
2023-04-11 18:03:49 -0400

    add ability to use a different model for optimization than for simulation

--- traj_opt/examples/example_base.h
+++ traj_opt/examples/example_base.h
@@ -5,6 +5,7 @@
 #include <string>
 #include <thread>
 #include <vector>
+#include <iostream> //debug
 
 #include "drake/common/find_resource.h"
 #include "drake/geometry/scene_graph.h"
@@ -71,6 +72,18 @@ class TrajOptExample {
    * @param plant the MultibodyPlant that we'll add the system to.
    */
   virtual void CreatePlantModel(MultibodyPlant<double>*) const {}
+
+  /**
+   * Create a MultibodyPlant model of the system to use for simulation (i.e., to
+   * test MPC). The default behavior is to use the same model that we use for
+   * optimization.
+   *
+   * @param plant the MultibodyPlant that we'll add the system to.
+   */
+  virtual void CreatePlantModelForSimulation(
+      MultibodyPlant<double>* plant) const {
+    CreatePlantModel(plant);
+  }
 
   /**
    * Play back the given trajectory on the Drake visualizer
--- traj_opt/examples/jaco.cc
+++ traj_opt/examples/jaco.cc
@@ -18,9 +18,7 @@ using multibody::Parser;
 
 class JacoExample : public TrajOptExample {
   void CreatePlantModel(MultibodyPlant<double>* plant) const final {
-    const Vector4<double> blue(0.1, 0.3, 0.5, 1.0);
     const Vector4<double> green(0.3, 0.6, 0.4, 1.0);
-    const Vector4<double> black(0.0, 0.0, 0.0, 1.0);
 
     // Add a jaco arm
     std::string robot_file = FindResourceOrThrow(
@@ -46,6 +44,33 @@ class JacoExample : public TrajOptExample {
     // N.B. When combined with the friction coefficient of the box according to
     // μ = 2μₘμₙ/(μₘ + μₙ), this gives a friction coefficient of roughly 0.1
     // between the box and the ground.
+  }
+
+  void CreatePlantModelForSimulation(
+      MultibodyPlant<double>* plant) const final {
+    const Vector4<double> green(0.3, 0.6, 0.4, 1.0);
+
+    // Add a jaco arm
+    std::string robot_file = FindResourceOrThrow(
+        "drake/traj_opt/examples/models/j2s7s300_arm_sphere_collision_v2.sdf");
+    ModelInstanceIndex jaco = Parser(plant).AddModelFromFile(robot_file);
+    RigidTransformd X_jaco(Vector3d(0, -0.27, 0.11));
+    plant->WeldFrames(plant->world_frame(), plant->GetFrameByName("base"),
+                      X_jaco);
+    plant->disable_gravity(jaco);
+
+    // Add a manipuland
+    std::string manipuland_file =
+        FindResourceOrThrow("drake/traj_opt/examples/models/box_15cm.sdf");
+    Parser(plant).AddAllModelsFromFile(manipuland_file);
+
+    // Add the ground
+    RigidTransformd X_ground(Vector3d(0.0, 0.0, -0.5));
+    plant->RegisterVisualGeometry(plant->world_body(), X_ground, Box(25, 25, 1),
+                                  "ground", green);
+    plant->RegisterCollisionGeometry(plant->world_body(), X_ground,
+                                     Box(25, 25, 1), "ground",
+                                     CoulombFriction<double>(0.5, 0.5));
   }
 };
 
