c264b86b8553d03f758fba8eb2bc685c656b1321
Vincent Kurtz <vjkurtz@gmail.com>
2025-01-21 15:01:26 -0600

    set up python script for testing

---
+++ bindings/pydrake/examples/multibody/integration_sanity_check.py
@@ -0,0 +1,7 @@
+from pydrake.geometry import Meshcat, Rgba
+from pydrake.math import RigidTransform, RotationMatrix
+from pydrake.multibody.plant import MultibodyPlant
+from pydrake.multibody.tree import FixedOffsetFrame
+from pydrake.planning import RobotDiagramBuilder
+
+print("hello world!")
