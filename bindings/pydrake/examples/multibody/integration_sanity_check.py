7f11084c8830eeb452c8708d853bbd86e0780362
Vincent Kurtz <vjkurtz@gmail.com>
2025-01-21 15:54:49 -0600

    add simple cylinder/table sim based on drake tutorials

--- bindings/pydrake/examples/multibody/integration_sanity_check.py
+++ bindings/pydrake/examples/multibody/integration_sanity_check.py
@@ -1,7 +1,144 @@
-from pydrake.geometry import Meshcat, Rgba
-from pydrake.math import RigidTransform, RotationMatrix
-from pydrake.multibody.plant import MultibodyPlant
-from pydrake.multibody.tree import FixedOffsetFrame
-from pydrake.planning import RobotDiagramBuilder
+import numpy as np
+from pydrake.geometry import SceneGraphConfig, StartMeshcat
+from pydrake.math import RigidTransform, RollPitchYaw
+from pydrake.multibody.parsing import Parser
+from pydrake.multibody.plant import AddMultibodyPlantSceneGraph
+from pydrake.systems.analysis import Simulator
+from pydrake.systems.framework import DiagramBuilder
+from pydrake.visualization import AddDefaultVisualization, ModelVisualizer
 
-print("hello world!")
+##
+#
+# Run some simple sanity checks on the possibility of error-controlled
+# integration with kLagged
+#
+##
+
+meshcat = StartMeshcat()
+
+# Model is defined in-line here
+cylinder_sdf = """<?xml version="1.0"?>
+<sdf version="1.7">
+  <model name="cylinder">
+    <pose>0 0 0 0 0 0</pose>
+    <link name="cylinder_link">
+      <inertial>
+        <mass>1.0</mass>
+        <inertia>
+          <ixx>0.005833</ixx>
+          <ixy>0.0</ixy>
+          <ixz>0.0</ixz>
+          <iyy>0.005833</iyy>
+          <iyz>0.0</iyz>
+          <izz>0.005</izz>
+        </inertia>
+      </inertial>
+      <collision name="collision">
+        <geometry>
+          <cylinder>
+            <radius>0.1</radius>
+            <length>0.2</length>
+          </cylinder>
+        </geometry>
+      </collision>
+      <visual name="visual">
+        <geometry>
+          <cylinder>
+            <radius>0.1</radius>
+            <length>0.2</length>
+          </cylinder>
+        </geometry>
+        <material>
+          <diffuse>1.0 1.0 1.0 1.0</diffuse>
+        </material>
+      </visual>
+    </link>
+  </model>
+</sdf>
+"""
+
+table_top_sdf = """<?xml version="1.0"?>
+<sdf version="1.7">
+  <model name="table_top">
+    <link name="table_top_link">
+      <visual name="visual">
+        <pose>0 0 0.445 0 0 0</pose>
+        <geometry>
+          <box>
+            <size>0.55 1.1 0.05</size>
+          </box>
+        </geometry>
+        <material>
+         <diffuse>0.9 0.8 0.7 1.0</diffuse>
+        </material>
+      </visual>
+      <collision name="collision">
+        <pose>0 0 0.445  0 0 0</pose>
+        <geometry>
+          <box>
+            <size>0.55 1.1 0.05</size>
+          </box>
+        </geometry>
+      </collision>
+    </link>
+    <frame name="table_top_center">
+      <pose relative_to="table_top_link">0 0 0.47 0 0 0</pose>
+    </frame>
+  </model>
+</sdf>
+"""
+
+def create_scene(sim_time_step):
+    # Clean up the Meshcat instance.
+    meshcat.Delete()
+    meshcat.DeleteAddedControls()
+
+    builder = DiagramBuilder()
+    plant, scene_graph = AddMultibodyPlantSceneGraph(
+        builder, time_step=sim_time_step)
+    parser = Parser(plant)
+
+    # Load the table top and the cylinder we created.
+    parser.AddModelsFromString(cylinder_sdf, "sdf")
+    parser.AddModelsFromString(table_top_sdf, "sdf")
+
+    # Weld the table to the world so that it's fixed during the simulation.
+    table_frame = plant.GetFrameByName("table_top_center")
+    plant.WeldFrames(plant.world_frame(), table_frame)
+
+    # Finalize the plant after loading the scene.
+    plant.Finalize()
+    # We use the default context to calculate the transformation of the table
+    # in world frame but this is NOT the context the Diagram consumes.
+    plant_context = plant.CreateDefaultContext()
+
+    # Set the initial pose for the free bodies, i.e., the custom cylinder,
+    # the cracker box, and the sugar box.
+    cylinder = plant.GetBodyByName("cylinder_link")
+    X_WorldTable = table_frame.CalcPoseInWorld(plant_context)
+    X_TableCylinder = RigidTransform(
+        RollPitchYaw(np.asarray([80, 0, 0]) * np.pi / 180), p=[0,0,0.5])
+    X_WorldCylinder = X_WorldTable.multiply(X_TableCylinder)
+    plant.SetDefaultFreeBodyPose(cylinder, X_WorldCylinder)
+    
+    # Add visualization to see the geometries.
+    AddDefaultVisualization(builder=builder, meshcat=meshcat)
+
+    diagram = builder.Build()
+    return diagram
+
+def initialize_simulation(diagram):
+    simulator = Simulator(diagram)
+    simulator.Initialize()
+    simulator.set_target_realtime_rate(1.)
+    return simulator
+
+def run_simulation(sim_time_step):
+    diagram = create_scene(sim_time_step)
+    simulator = initialize_simulation(diagram)
+    meshcat.StartRecording()
+    simulator.AdvanceTo(2.0)
+    meshcat.StopRecording()
+    meshcat.PublishRecording()
+
+run_simulation(sim_time_step=0.001)
