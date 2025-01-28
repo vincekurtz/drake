676b190a16cc8d53751087e1c0d3e8895e5b7120
Vincent Kurtz <vjkurtz@gmail.com>
2025-01-28 15:47:23 -0600

    convex integrator isolates plant

--- systems/analysis/convex_integrator.cc
+++ systems/analysis/convex_integrator.cc
@@ -1,4 +1,45 @@
 #include "drake/systems/analysis/convex_integrator.h"
 
+namespace drake {
+namespace systems {
+
+/**
+ * Integrates the system forward in time by h, starting at the current time t₀.
+ * This value of h is determined by IntegratorBase::Step().
+ */
+template <class T>
+bool ConvexIntegrator<T>::DoStep(const T& h) {
+  Context<T>& context = *this->get_mutable_context();
+
+  // CAUTION: This is performance-sensitive inner loop code that uses dangerous
+  // long-lived references into state and cache to avoid unnecessary copying and
+  // cache invalidation. Be careful not to insert calls to methods that could
+  // invalidate any of these references before they are used.
+
+  // Evaluate derivative xcdot₀ ← xcdot(t₀, x(t₀), u(t₀)).
+  const ContinuousState<T>& xc_deriv = this->EvalTimeDerivatives(context);
+  const VectorBase<T>& xcdot0 = xc_deriv.get_vector();
+
+  // Cache: xcdot0 references the live derivative cache value, currently
+  // up to date but about to be marked out of date. We do not want to make
+  // an unnecessary copy of this data.
+
+  // Update continuous state and time. This call marks t- and xc-dependent
+  // cache entries out of date, including xcdot0.
+  VectorBase<T>& xc = context.SetTimeAndGetMutableContinuousStateVector(
+      context.get_time() + h);  // t ← t₀ + h
+
+  // Cache: xcdot0 still references the derivative cache value, which is
+  // unchanged, although it is marked out of date.
+
+  xc.PlusEqScaled(h, xcdot0);  // xc(t₀ + h) ← xc(t₀) + h * xcdot₀
+
+  // This integrator always succeeds at taking the step.
+  return true;
+}
+
+}  // namespace systems
+}  // namespace drake
+
 DRAKE_DEFINE_CLASS_TEMPLATE_INSTANTIATIONS_ON_DEFAULT_SCALARS(
     class drake::systems::ConvexIntegrator);
--- systems/analysis/convex_integrator.h
+++ systems/analysis/convex_integrator.h
@@ -4,10 +4,13 @@
 
 #include "drake/common/default_scalars.h"
 #include "drake/common/drake_copyable.h"
+#include "drake/multibody/plant/multibody_plant.h"
 #include "drake/systems/analysis/integrator_base.h"
 
 namespace drake {
 namespace systems {
+
+using multibody::MultibodyPlant;
 
 /**
  * An experimental implicit integrator that solves a convex SAP problem to
@@ -24,8 +27,9 @@ class ConvexIntegrator final : public IntegratorBase<T> {
   ~ConvexIntegrator() override = default;
 
   /**
-   * Constructs a fixed-step integrator for a given system using the given
-   * context for initial conditions.
+   * Constructs the experimental convex integrator. For now this only supports
+   * a simple MultibodyPlant, unconnected to anything else.
+   *
    * @param system A reference to the system to be simulated
    * @param max_step_size The maximum (fixed) step size; the integrator will
    *                      not take larger step sizes than this.
@@ -35,57 +39,36 @@ class ConvexIntegrator final : public IntegratorBase<T> {
    * @sa Initialize()
    */
   ConvexIntegrator(const System<T>& system, const T& max_step_size,
-                          Context<T>* context = nullptr)
+                   Context<T>* context = nullptr)
       : IntegratorBase<T>(system, context) {
     IntegratorBase<T>::set_maximum_step_size(max_step_size);
+
+    // Check that the system we're simulating is a diagram with a plant in it
+    const Diagram<T>* diagram = dynamic_cast<const Diagram<T>*>(&system);
+    DRAKE_ASSERT(diagram != nullptr);
+
+    // Isolate the MultibodyPlant subsystem
+    plant_ = dynamic_cast<const MultibodyPlant<T>*>(
+        &diagram->GetSubsystemByName("plant"));
+    DRAKE_ASSERT(plant_ != nullptr);
+
+    // TODO(vincekurtz): consider additional checks to ensure that there are no
+    // significant systems other than the plant in the diagram.
   }
 
-  /**
-   * Explicit Euler integrator does not support error estimation.
-   */
+  // TODO(vincekurtz): add error estimation
   bool supports_error_estimation() const override { return false; }
 
-  /// Integrator does not provide an error estimate.
+  // TODO(vincekurtz): add error estimation
   int get_error_estimate_order() const override { return 0; }
 
  private:
+  // The primary integration step, sets x_{t+h}
   bool DoStep(const T& h) override;
+
+  // Plant model, since convex integration is specific to MbP
+  const MultibodyPlant<T>* plant_;
 };
-
-/**
- * Integrates the system forward in time by h, starting at the current time t₀.
- * This value of h is determined by IntegratorBase::Step().
- */
-template <class T>
-bool ConvexIntegrator<T>::DoStep(const T& h) {
-  Context<T>& context = *this->get_mutable_context();
-
-  // CAUTION: This is performance-sensitive inner loop code that uses dangerous
-  // long-lived references into state and cache to avoid unnecessary copying and
-  // cache invalidation. Be careful not to insert calls to methods that could
-  // invalidate any of these references before they are used.
-
-  // Evaluate derivative xcdot₀ ← xcdot(t₀, x(t₀), u(t₀)).
-  const ContinuousState<T>& xc_deriv = this->EvalTimeDerivatives(context);
-  const VectorBase<T>& xcdot0 = xc_deriv.get_vector();
-
-  // Cache: xcdot0 references the live derivative cache value, currently
-  // up to date but about to be marked out of date. We do not want to make
-  // an unnecessary copy of this data.
-
-  // Update continuous state and time. This call marks t- and xc-dependent
-  // cache entries out of date, including xcdot0.
-  VectorBase<T>& xc = context.SetTimeAndGetMutableContinuousStateVector(
-      context.get_time() + h);  // t ← t₀ + h
-
-  // Cache: xcdot0 still references the derivative cache value, which is
-  // unchanged, although it is marked out of date.
-
-  xc.PlusEqScaled(h, xcdot0);   // xc(t₀ + h) ← xc(t₀) + h * xcdot₀
-
-  // This integrator always succeeds at taking the step.
-  return true;
-}
 
 }  // namespace systems
 }  // namespace drake
