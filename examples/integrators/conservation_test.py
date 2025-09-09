399a7bdb79a6c006182c5189c3c734952b48b521
Vincent Kurtz <vjkurtz@gmail.com>
2025-09-09 10:07:59 -0500

    add more interesting conservation test

---
+++ examples/integrators/conservation_test.py
@@ -0,0 +1,154 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from pydrake.all import (
+    MultibodyPlant,
+    DiagramBuilder,
+    AddMultibodyPlantSceneGraph,
+    Simulator,
+    PrintSimulatorStatistics,
+    SimulatorConfig,
+    ApplySimulatorConfig,
+    Parser,
+    AddDefaultVisualization,
+    SceneGraphConfig,
+    StartMeshcat,
+)
+
+undamped_double_pendulum_xml = """
+<?xml version="1.0"?>
+<mujoco model="double_pendulum">
+<worldbody>
+  <body>
+  <joint type="free"/>
+  <geom type="capsule" size="0.01 0.1"/>
+  <body>
+    <joint type="hinge" axis="0 1 0" pos="0 0 -0.1" damping="0.0"/>
+    <geom type="capsule" size="0.01 0.1" pos="0 0 -0.2"/>
+  </body>
+  </body>
+</worldbody>
+</mujoco> 
+"""
+
+sphere_xml = """
+<?xml version="1.0"?>
+<mujoco model="sphere">
+<worldbody>
+  <body>
+    <joint type="free"/>
+    <geom type="sphere" size="0.03"/>
+  </body>
+</worldbody>
+</mujoco>
+"""
+
+def run_conservation_test(time_step, integration_scheme, convex_scheme, visualize=False):
+    """Simulate a double pendulum floating in space, hit by a sphere.
+
+    Record energy and (linear, angular) momentum at the beginning and end of the
+    simulation, and report conservation errors. This will help us to measure the
+    order of various integration schemes. 
+    """
+    builder = DiagramBuilder()
+    plant, scene_graph = AddMultibodyPlantSceneGraph(builder, time_step=0.0)
+
+    # Double pendulum model
+    Parser(plant).AddModelsFromString(undamped_double_pendulum_xml, "xml")
+
+    # Sphere model
+    Parser(plant).AddModelsFromString(sphere_xml, "xml")
+
+    # Turn off gravity
+    plant.mutable_gravity_field().set_gravity_vector([0, 0, 0])
+    plant.Finalize()
+
+    # Elastic collisions (zero dissipation)
+    sg_config = SceneGraphConfig()
+    sg_config.default_proximity_properties.hunt_crossley_dissipation = 0.0
+    sg_config.default_proximity_properties.static_friction = 0.0
+    sg_config.default_proximity_properties.dynamic_friction = 0.0
+    sg_config.default_proximity_properties.point_stiffness = 1e3
+    scene_graph.set_config(sg_config)
+
+    # Optional visualization
+    if visualize:
+        meshcat = StartMeshcat()
+        AddDefaultVisualization(builder=builder, meshcat=meshcat)
+
+    diagram = builder.Build()
+
+    # Set up the simulator
+    simulator = Simulator(diagram)
+    config = SimulatorConfig()
+    config.integration_scheme = integration_scheme
+    config.max_step_size = time_step
+    config.use_error_control = False
+    config.publish_every_time_step = True
+    config.target_realtime_rate = 0.0
+    ApplySimulatorConfig(config, simulator)
+
+    if config.integration_scheme == "convex":
+        ci = simulator.get_mutable_integrator()
+        ci.set_plant(plant)
+        ci_params = ci.get_solver_parameters()
+        ci_params.error_estimation_strategy = convex_scheme
+        ci.set_solver_parameters(ci_params)
+
+    # Set initial conditions
+    context = simulator.get_mutable_context()
+    plant_context = plant.GetMyMutableContextFromRoot(context)
+    q0 = plant.GetPositions(plant_context)
+    q0[-3:] = [0.5, 0.0, 0.1]  # sphere position
+    v0 = plant.GetVelocities(plant_context)
+    v0[-3:] = [-1.0, 0.0, 0.0]  # sphere velocity
+    plant.SetPositions(plant_context, q0)
+    plant.SetVelocities(plant_context, v0)
+    simulator.Initialize()
+
+    # Compute initial conserved quantities
+    initial_energy = plant.CalcKineticEnergy(plant_context)
+    p_CoM = plant.CalcCenterOfMassPositionInWorld(plant_context)
+    initial_momentum = plant.CalcSpatialMomentumInWorldAboutPoint(
+        plant_context, p_CoM
+    )
+
+    if visualize:
+        input("waiting for meshcat, [ENTER] to continue...")
+        meshcat.StartRecording()
+
+    simulator.AdvanceTo(5.0)
+    if visualize:
+        meshcat.StopRecording()
+        meshcat.PublishRecording()
+        PrintSimulatorStatistics(simulator)
+
+    final_energy = plant.CalcKineticEnergy(plant_context)
+    final_momentum = plant.CalcSpatialMomentumInWorldAboutPoint(
+        plant_context, p_CoM
+    )
+
+    energy_error = np.abs(final_energy - initial_energy)
+    linear_momentum_error = np.linalg.norm(
+        final_momentum.translational() - initial_momentum.translational()
+    )
+    angular_momentum_error = np.linalg.norm(
+        final_momentum.rotational() - initial_momentum.rotational()
+    )
+
+    name = f"{integration_scheme}"
+    if integration_scheme == "convex":
+        name += f" ({convex_scheme})"
+
+    print(f"Integration scheme: {name}")
+    print(f"Time step: {time_step}")
+    print(f"Energy error: {energy_error}")
+    print(f"Linear momentum error: {linear_momentum_error}")
+    print(f"Angular momentum error: {angular_momentum_error}")
+    print("")
+
+    return energy_error, linear_momentum_error, angular_momentum_error
+
+if __name__ == "__main__":
+    run_conservation_test(0.0001, "convex", "midpoint", visualize=False)
+
